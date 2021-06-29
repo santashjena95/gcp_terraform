71105a87d3ada5836ab957a0f3993bc9bc80765d
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-06-29 13:26:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,19 @@
-variable "instance_name" {
+variable "vm_internal_ip" {
+  description = "Internal IP Address of VM Instance"
+  type        = string
+  default     = "10.0.0.20"
+}
+variable "vm_region_name" {
+  description = "Region location of VM Instance"
+  type        = string
+  default     = "us-east4"
+}
+variable "vm_instance_name" {
   description = "The name of VM Instance"
   type        = string
   default     = "sles15sp1sap20"
 }
-variable "vm_zone" {
+variable "vm_zone_name" {
   description = "Zone location of VM Instance"
   type        = string
   default     = "us-east4-c"
