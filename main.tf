2f98eac4924aef36f58713f9dfa7bcbd0355eb75
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-06-29 17:42:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "gce_instance_1" {
   #vm_machine_type     = ""
   vm_zone             = var.vm_zone_name
   #vm_image            = ""
-  service_account     = "${module.service_account_1.email}"
+  service_account     = "${module.service_account_1.google_service_account.email}"
   startup_script = <<SCRIPT
       #! /bin/bash
       sudo hostnamectl set-hostname ${var.vm_instance_name}
