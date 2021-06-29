5231ca439eb142844b06ce4f14f63611af316edb
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-06-29 13:25:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ module "gce_instance_1" {
   internal_ip_name    = "my-internal-test"
   network_name        = "test-vpc"
   subnetwork_name     = "new-us-east"
-  internal_ip_address = "10.0.0.20"
-  #vm_region           = ""
-  instance_name       = "sles15sp1sap20"
+  internal_ip_address = var.vm_internal_ip
+  vm_region           = var.vm_region_name
+  instance_name       = var.vm_instance_name
   #vm_machine_type     = ""
-  #vm_zone             = ""
+  vm_zone             = var.vm_zone_name
   #vm_image            = ""
   service_account     = "terraform@industrial-pad-316908.iam.gserviceaccount.com"
   startup_script = <<SCRIPT
       #! /bin/bash
-      sudo sed -i 's/.*127.0.1.1.*/${google_compute_address.internal_ip.address} ${var.instance_name}.personallab.local ${var.instance_name}/' /etc/hosts
-      sudo hostnamectl set-hostname ${var.instance_name}
+      sudo hostnamectl set-hostname ${var.vm_instance_name}
+      sudo sed -i 's/.*127.0.1.1.*/${var.vm_internal_ip} ${var.vm_instance_name}.personallab.local ${var.vm_instance_name}/' /etc/hosts
       echo ${var.domain_password} | kinit -V ${var.domain_user}@PERSONALLAB.LOCAL
       echo ${var.domain_password} | sudo realm join --verbose --user=${var.domain_user} PERSONALLAB.LOCAL
       sudo realm permit -g AccAdminSecOpsServers@PERSONALLAB.LOCAL
@@ -25,6 +25,6 @@ module "gce_instance_1" {
       SCRIPT
   shutdown_script = <<SCRIPT
       #! /bin/bash
-      /home/jenasantash95/google-cloud-sdk/bin/gcloud compute instances remove-metadata ${var.instance_name} --zone=${var.vm_zone} --keys=startup-script,shutdown-script
+      /home/jenasantash95/google-cloud-sdk/bin/gcloud compute instances remove-metadata ${var.vm_instance_name} --zone=${var.vm_zone_name} --keys=startup-script,shutdown-script
       SCRIPT 
 }
