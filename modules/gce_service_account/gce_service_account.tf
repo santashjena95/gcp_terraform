a851f5000e96a27c84469001e271d547f781e8a1
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-06-29 19:21:05 +0530

    Update gce_service_account.tf

--- modules/gce_service_account/gce_service_account.tf
+++ modules/gce_service_account/gce_service_account.tf
@@ -2,3 +2,13 @@ resource "google_service_account" "service_account" {
   account_id   = var.account_id
   display_name = var.display_name
 }
+resource "google_project_iam_custom_role" "custom_role" {
+  role_id     = "vmCustomRole"
+  title       = "VM Custom Role"
+  description = "Role for giving metadata level access to service account"
+  permissions = ["compute.instances.setMetadata"]
+}
+resource "google_project_iam_member" "project" {
+  role    = google_project_iam_custom_role.custom_role.name
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
