6e68610a1d00c87b0a2d1c354303f9f0e74349a6
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-06-29 16:13:27 +0530

    Update gce_network.tf

--- modules/gce_network/gce_network.tf
+++ modules/gce_network/gce_network.tf
@@ -2,7 +2,7 @@ resource "google_compute_network" "vpc_network_creation" {
   name                    = var.vpc_network
   mtu                     = 1460
   auto_create_subnetworks = false
-  routing_mode            = REGIONAL
+  routing_mode            = "REGIONAL"
 }
 resource "google_compute_subnetwork" "vpc_subnet_creation" {
   name                     = var.vpc_subnet
