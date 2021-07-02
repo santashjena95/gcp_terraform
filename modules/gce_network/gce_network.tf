ab4f60d8ec22e76b5fbf535dec05395f82241ec6
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-07-02 20:17:08 +0530

    Update gce_network.tf

--- modules/gce_network/gce_network.tf
+++ modules/gce_network/gce_network.tf
@@ -7,7 +7,7 @@ resource "google_compute_network" "vpc_network_creation" {
 resource "google_compute_subnetwork" "vpc_subnet_creation" {
   name                     = var.vpc_subnet
   ip_cidr_range            = var.cidr_range
-  region                   = var.region
+  region                   = var.subnet_region
   private_ip_google_access = true
   network                  = google_compute_network.vpc_network_creation.id
 }
